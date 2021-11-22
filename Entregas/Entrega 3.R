0f0291aaf105ab7ad11c77f149f0bb79b52ffae5
Arratia-Val <89551722+Arratia-Val@users.noreply.github.com>
2021-11-22 11:15:23 -0300

    Add files via upload
    Subo Entrega 3

---
+++ Entregas/Entrega 3.R
@@ -0,0 +1,34 @@
+#ENTREGA 3
+
+#EJ 1
+pnorm(0.25819)-pnorm(-0.344265)
+
+pnorm(0.25819)+pnorm(0.344265)-1
+
+pnorm(18/((sqrt(60))*9)+pnorm(24/((sqrt(60))*9)-1
+                              
+pnorm(18/(sqrt(60)*9)-pnorm(-24/(sqrt(60)*9)
+                                                          
+pgamma(-58.966666666,60,9)-pgamma(-59.044444444,60,9)
+                                                          
+pgamma(558,shape=60,scale=9)-pgamma(516,shape=60,scale=9)
+pgamma(558,shape=60,scale=9)-pgamma(516,shape=60,scale=9)
+                                                          
+pgamma(0.25819,scale=60,9)-pgamma(-0.344265,60,9)
+                                                          
+#EJ 2
+                                                          
+ppois(28,19)-ppois(9,19)
+                                                          
+#EJ 3
+datos <-c(5.2, 7.3, 4.7, 8.3, 8.6, 7.6, 4.8, 9.6, 4.1, 1.5, 10.4)
+                                                          
+mean(datos)
+                                                          
+median(datos)
+                                                          
+var(datos)
+                                                          
+sum(datos > 6)/11
+                                                          
+sum(datos > 6)/length(datos)
